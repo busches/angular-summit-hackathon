5fdd14ef13448381ade1fc015c230f6f7a6c01d8
Scott Busche <swbusche@gmail.com>
2016-07-20 20:11:53 -0500

    Remove about.js and test files that test nothing

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,7 +22,6 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
@@ -32,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-route-segment/build/angular-route-segment.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/moment/moment.js',
       'bower_components/angular-mocks/angular-mocks.js',
